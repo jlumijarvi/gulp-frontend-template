83a51efb311573a2278675bfc5a4ef189e14f19f
Janne Lumijärvi <Janne Lumijrvi>
2015-11-12 22:28:24 +0200

    cleaning...

--- build/app/app.js
+++
@@ -1,7 +0,0 @@
-/// <reference path="../../typings/tsd.d.ts" />
-(function () {
-    $(document).ready(function () {
-        console.log('I\'m ready!');
-        console.log('Timestamp: ' + new Date().toUTCString());
-    });
-})();
